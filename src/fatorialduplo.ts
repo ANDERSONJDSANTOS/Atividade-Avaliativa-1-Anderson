513b1a3695e540573a560dc68413f2a38663b2df
ANDERSON DE JESUS DOS SANTOS <81601551+ANDERSONJDSANTOS@users.noreply.github.com>
2022-03-10 21:34:09 -0300

    Add files via upload

---
+++ src/fatorialduplo.ts
@@ -0,0 +1,14 @@
+export function calculaFatorialDuploPositivo(numero: number): any{
+    if (numero == 1){
+        return 1;
+    }else{
+        if(numero ==0){
+            return 0;
+        }
+    }
+    return numero * calculaFatorialDuploPositivo(numero -2);
+    
+    
+
+}
+console.log(calculaFatorialDuploPositivo(0));
